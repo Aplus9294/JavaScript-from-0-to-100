c9335a0cec079508c3f5ba5c5a3c747a9ca7ce81
Administrator <Administrator@A9294Computer>
2017-05-10 07:18:55 +0800

    Change-Id: Ie8c37adb17a357f5ce7fcec8457a4e9c7c65e46f Signed-off-by: Administrator <Administrator@A9294Computer>

--- JavaScript/WebContent/24/mrsoft/js/floatdiv.js
+++ JavaScript/WebContent/24/mrsoft/js/floatdiv.js
@@ -1,4 +1,4 @@
-﻿var delta=0.15
+﻿var delta = 0.15
 var collection;
 var html = '<table width="81" border="0" cellspacing="0" cellpadding="0">\
   <tr>\
@@ -23,59 +23,70 @@ var html = '<table width="81" border="0" cellspacing="0" cellpadding="0">\
 </table>';
 
 function floaters() {
-	this.items	= [];
-	this.addItem	= function(id,x,y,content) {
-		document.write('<DIV id='+id+' style="Z-INDEX: 10; POSITION: absolute;width:80px; right:30px;right:'+(typeof(x)=='string'?eval(x):x)+';top:'+(typeof(y)=='string'?eval(y):y)+'">'+content+'</DIV>');
-		
+	this.items = [];
+	this.addItem = function(id, x, y, content) {
+		document
+				.write('<DIV id='
+						+ id
+						+ ' style="Z-INDEX: 10; POSITION: absolute;width:80px; right:30px;right:'
+						+ (typeof (x) == 'string' ? eval(x) : x) + ';top:'
+						+ (typeof (y) == 'string' ? eval(y) : y) + '">'
+						+ content + '</DIV>');
+
 		var newItem = {};
 		newItem.object = document.getElementById(id);
 		newItem.x = x;
 		newItem.y = y;
- 
-		this.items[this.items.length]		= newItem;
+
+		this.items[this.items.length] = newItem;
 	}
 	this.play = function() {
 		collection = this.items
-		setInterval('play()',10);
+		setInterval('play()', 10);
 	}
 }
 
-
 function play() {
-	var width = document.documentElement.clientWidth||document.body.clientWidth;
-var height = document.documentElement.clientHeight||document.body.clientHeight;
-if ( width > 200 )
-	theFloaters.items[0].x = width -100;
- 
-if ( height > 300 )
-	theFloaters.items[0].y = height -400;
-	if(screen.width<=800) {
-		for(var i=0;i<collection.length;i++) {
-			collection[i].object.style.display	= 'none';
+	var width = document.documentElement.clientWidth
+			|| document.body.clientWidth;
+	var height = document.documentElement.clientHeight
+			|| document.body.clientHeight;
+	if (width > 200)
+		theFloaters.items[0].x = width - 100;
+
+	if (height > 300)
+		theFloaters.items[0].y = height - 400;
+	if (screen.width <= 800) {
+		for (var i = 0; i < collection.length; i++) {
+			collection[i].object.style.display = 'none';
 		}
 		return;
 	}
-	for(var i=0;i<collection.length;i++) {
-		var followObj		= collection[i].object;
-		var followObj_x		= (typeof(collection[i].x)=='string'?eval(collection[i].x):collection[i].x);
-		var followObj_y		= (typeof(collection[i].y)=='string'?eval(collection[i].y):collection[i].y);
- 
-		if(followObj.offsetLeft!=(document.body.scrollLeft+followObj_x)) {
-			var dx=(document.body.scrollLeft+followObj_x-followObj.offsetLeft)*delta;
-			dx=(dx>0?1:-1)*Math.ceil(Math.abs(dx));
-			followObj.style.left=(followObj.offsetLeft+dx)+"px";
-			}
-                var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-		if(followObj.offsetTop!=(scrollTop+followObj_y)) {
-			var dy=(scrollTop+followObj_y-followObj.offsetTop)*delta;
-			dy=(dy>0?1:-1)*Math.ceil(Math.abs(dy));
-			followObj.style.top=followObj.offsetTop+dy+"px";
-			}
-		followObj.style.display	= '';
+	for (var i = 0; i < collection.length; i++) {
+		var followObj = collection[i].object;
+		var followObj_x = (typeof (collection[i].x) == 'string' ? eval(collection[i].x)
+				: collection[i].x);
+		var followObj_y = (typeof (collection[i].y) == 'string' ? eval(collection[i].y)
+				: collection[i].y);
+
+		if (followObj.offsetLeft != (document.body.scrollLeft + followObj_x)) {
+			var dx = (document.body.scrollLeft + followObj_x - followObj.offsetLeft)
+					* delta;
+			dx = (dx > 0 ? 1 : -1) * Math.ceil(Math.abs(dx));
+			followObj.style.left = (followObj.offsetLeft + dx) + "px";
+		}
+		var scrollTop = window.pageYOffset
+				|| document.documentElement.scrollTop
+				|| document.body.scrollTop || 0;
+		if (followObj.offsetTop != (scrollTop + followObj_y)) {
+			var dy = (scrollTop + followObj_y - followObj.offsetTop) * delta;
+			dy = (dy > 0 ? 1 : -1) * Math.ceil(Math.abs(dy));
+			followObj.style.top = followObj.offsetTop + dy + "px";
+		}
+		followObj.style.display = '';
 	}
 }
 var theFloaters = new floaters();
-theFloaters.addItem('followDiv2',30,80,html);
-
+theFloaters.addItem('followDiv2', 30, 80, html);
 
 theFloaters.play();
