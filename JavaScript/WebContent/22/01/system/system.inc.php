11ea3389d58ad699150b2cb1b634504a9642f829
Administrator <Administrator@A9294Computer>
2017-05-13 12:14:58 +0800

    Change-Id: I143dd2f3cfaa93f9eeafe23a6328ab7a79f17a2d Signed-off-by: Administrator <Administrator@A9294Computer>

--- JavaScript/WebContent/22/01/system/system.inc.php
+++ JavaScript/WebContent/22/01/system/system.inc.php
@@ -1,7 +1,7 @@
 <?php
 require("system.smarty.inc.php");		//包含类文件
 require("system.class.inc.php");		//包含Smarty模板文件
-$connobj=new ConnDB("mysql","localhost","root","root","db_database04",false);	//数据库连接类实例化
+$connobj=new ConnDB("mysql","localhost","root","78711116814ZKybb","db_database04",false);	//数据库连接类实例化
 $conn=$connobj->GetConnId();		//执行数据库连接方法
 $admindb=new AdminDB();			//数据库操作类实例化
 $smarty=new SmartyProject();		//实例化Smarty配置类
